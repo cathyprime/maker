9daa09cb17464b5136941e96e88b7c1daf9bcd05
cathyprime <magdalena.mjaroszynska@gmail.com>
2024-12-24 22:51:57 +0100

    Remove command builder

--- maker.hh
+++ maker.hh
@@ -21,56 +21,6 @@
 
 namespace maker {
 
-template<std::size_t size, typename CharT = char, typename Traits = std::char_traits<CharT>>
-class Cmd_Builder {
-    using StringType = std::basic_string<CharT, Traits>;
-    std::array<StringType, size> m_cmd;
-    std::size_t m_idx;
-
-  public:
-    constexpr Cmd_Builder() = default;
-
-    template <typename... Args, typename = std::enable_if_t<(std::is_convertible_v<Args, StringType> && ...)>>
-    constexpr Cmd_Builder(Args... args) noexcept
-        : m_cmd()
-        , m_idx(0)
-    {
-        static_assert(sizeof...(args) <= size, "Too many arguments for Cmd_Builder");
-        StringType strs[] = {args...};
-        for (auto &s : strs) {
-            m_cmd[m_idx++] = std::move(s);
-        }
-    }
-    template<typename... Args, typename = std::enable_if_t<(std::is_convertible_v<Args, StringType> && ...)>>
-    constexpr Cmd_Builder &push(Args... args) noexcept
-    {
-        static_assert(sizeof...(args) <= size, "Too many arguments for Cmd_Builder");
-        StringType strs[] = {args...};
-        for (auto &s : strs) {
-            if (m_idx < size) {
-                m_cmd[m_idx++] = std::move(s);
-            }
-        }
-        return *this;
-    }
-    constexpr Cmd_Builder &push(StringType &&s) noexcept
-    {
-        if (m_idx < size) {
-            m_cmd[m_idx++] = std::move(s);
-        }
-        return *this;
-    }
-    [[nodiscard]] std::string build() const noexcept
-    {
-        std::string result = m_cmd[0];
-
-        for (auto it = m_cmd.begin() + 1; it != m_cmd.end(); ++it) {
-            result += ' ' + *it;
-        }
-        return result;
-    }
-};
-
 struct Rule {
     std::vector<std::string> deps;
     std::string target;
